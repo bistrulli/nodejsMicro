cf16ff838a246e9d95d335a805f36dfd42fe0124
emilio <emilio@mpbwalt.local>
2022-07-25 20:36:02 +0200

    minor

--- fitRT/simQN/test.m
+++ fitRT/simQN/test.m
@@ -1,8 +1,8 @@
 clear
 
-X0=[0,300,0];
-MU=[1,1,1];
-S=[inf,1,2];
+X0=[10,0,0];
+MU=[80,5,30];
+S=[inf,4,4];
 P=[0 1. 0 ;
    0 0  1.;
    1. 0 0];
@@ -10,6 +10,6 @@ TF=2000;
 dt=0.1;
 rep=100;
 
-% X=QN(X0,S,P,MU,TF,rep,dt);
-% Xm=mean(X,3);
-[t,y,T,RT]=QN_ODE(X0,S,P,MU);
+X=QN(X0,S,P,MU,TF,rep,dt);
+Xm=mean(X,3);
+[t,y,T,RT]=QN_ODE(X0,S,P,MU,inf);
