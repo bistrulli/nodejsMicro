411465ab5baedd71fab32ad2bceb9d949dea667f
emilio <emilio@mpbwalt.local>
2022-07-26 16:28:29 +0200

    minor

--- fitRT/simQN/joinExp.m
+++ fitRT/simQN/joinExp.m
@@ -12,6 +12,7 @@ Cli=cat(2,e1.Cli([1:e1CIdx]),e2.Cli(1:e2CIdx));
 RTm=cat(1,e1.RTm([1:e1CIdx],:),e2.RTm([1:e2CIdx],:));
 Tm=cat(1,e1.Tm([1:e1CIdx],:),e2.Tm([1:e2CIdx],:));
 NC=cat(1,e1.NC([1:e1CIdx],:),e2.NC([1:e2CIdx],:));
+ms=e1.ms
 
 
 clear e1 e2
