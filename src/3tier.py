6372bad1115f18f129fc8831a36992ec3fc9b7dd
emilio <emilio@mpbwalt.local>
2022-07-26 11:57:59 +0200

    exp

--- src/3tier.py
+++ src/3tier.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
                 data["trCI"][-1].append(sys.data[ms]["tr"][1])
                 
             print("####pop %d converged###" % (p))
-            savemat("../data/3tier_test_java_jitless_worker_2.mat", data)
+            savemat("../data/3tier_test_java_jitless_worker_3.mat", data)
             
             print("killing clients")
             sys.stopClient()
