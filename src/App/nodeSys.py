372b5f696592a78daf802531d3bcc6dcb616d30a
emilio <emilio@mpbwalt.local>
2022-07-29 15:20:03 +0200

    minor

--- src/App/nodeSys.py
+++ src/App/nodeSys.py
@@ -31,7 +31,7 @@ class nodeSys():
         self.mntThreads=[]
         self.data={}
         self.startTime=None
-        self.clearLog()
+        #self.clearLog()
     
     
     def startSys(self):
