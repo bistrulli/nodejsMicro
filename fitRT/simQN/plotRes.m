00bcc694d34e44296b5097cd835217364749de61
emilio <emilio@mpbwalt.local>
2022-08-15 09:55:12 +0200

    minor

--- fitRT/simQN/plotRes.m
+++ fitRT/simQN/plotRes.m
@@ -1,6 +1,6 @@
 clear
 load("/Users/emilio/git/MS-App/fitLQN/fromJulia.mat")
-load("/Users/emilio/git/nodejsMicro/data/4tier_spring.py_wi2.mat");
+load("/Users/emilio/git/nodejsMicro/data/4tier_spring.py_wi3.mat");
 
 % P=[0,1,0;
 %    0,0,1;
