2f176fe00268986729c2c8821897dc10a4a82c46
emilio <emilio@mpbwalt.local>
2022-09-13 08:56:09 +0200

    minor

--- src/acmeAir.py
+++ src/acmeAir.py
@@ -109,37 +109,37 @@ if __name__ == '__main__':
             print("####pop %d###" % (p))
             
             sys.startSys(msSys=msSys)
-            time.sleep(5)
-            sys.startClient(p)
-            sys.startMNT()
-            
-            data["ms"] = list(sys.data.keys())
-            data["RTm"].append([])
-            data["Tm"].append([])
-            data["rtCI"].append([])
-            data["trCI"].append([])
-            data["NC"].append([])
-            
-            for ms in  data["ms"]:
-                data["RTm"][-1].append(sys.data[ms]["rt"][0])
-                data["Tm"][-1].append(sys.data[ms]["tr"][0])
-            
-                data["rtCI"][-1].append(sys.data[ms]["rt"][1])
-                data["trCI"][-1].append(sys.data[ms]["tr"][1])
-            
-                if(ms=="client"):
-                    data["NC"][-1].append(1000)
-                else:
-                    data["NC"][-1].append(msSys[ms]["hw"])
-            
-            print("####pop %d converged###" % (p))
-            savemat("../data/%s_wi.mat"%(os.path.basename(__file__)), data)
-            
-            print("killing clients")
-            sys.stopClient()
-            print("killing system") 
-            sys.stopSys()
-            sys.reset()
+            # time.sleep(5)
+            # sys.startClient(p)
+            # sys.startMNT()
+            #
+            # data["ms"] = list(sys.data.keys())
+            # data["RTm"].append([])
+            # data["Tm"].append([])
+            # data["rtCI"].append([])
+            # data["trCI"].append([])
+            # data["NC"].append([])
+            #
+            # for ms in  data["ms"]:
+            #     data["RTm"][-1].append(sys.data[ms]["rt"][0])
+            #     data["Tm"][-1].append(sys.data[ms]["tr"][0])
+            #
+            #     data["rtCI"][-1].append(sys.data[ms]["rt"][1])
+            #     data["trCI"][-1].append(sys.data[ms]["tr"][1])
+            #
+            #     if(ms=="client"):
+            #         data["NC"][-1].append(1000)
+            #     else:
+            #         data["NC"][-1].append(msSys[ms]["hw"])
+            #
+            # print("####pop %d converged###" % (p))
+            # savemat("../data/%s_wi.mat"%(os.path.basename(__file__)), data)
+            #
+            # print("killing clients")
+            # sys.stopClient()
+            # print("killing system") 
+            # sys.stopSys()
+            # sys.reset()
     
     except Exception as ex:
         print("Error")
