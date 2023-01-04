3d34fd12f179522aecc16a6e1bacb4d804fe1c66
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-04 13:33:56 +0100

    minor

--- src/Client/laoadShapeAcme_step.py
+++ src/Client/laoadShapeAcme_step.py
@@ -7,8 +7,8 @@ class loadShapeAcme_step(loadShape):
         super().__init__(maxt,sys)
         
     def gen(self):
-        if((self.t) % 10000==0):
-            return self.sys.userCount+20
+        if((self.t) % 30==0):
+            return self.sys.userCount+30
         else:
             return self.sys.userCount
     
