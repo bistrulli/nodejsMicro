f958b0bd0665c34798b0e8a4affa03cb78403384
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-19 14:04:31 +0100

    minor

--- src/Client/clientProcess.py
+++ src/Client/clientProcess.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 
-from pymongo import MongoClient
+#from pymongo import MongoClient
 import multiprocessing
 
 
@@ -11,8 +11,8 @@ class clientProcess(multiprocessing.Process):
     id = None
     mongoClient = None
     ttime = None
-    dbHost="185.154.155.43"
-    remoteHost="185.154.155.43"
+    dbHost="185.10.16.192"
+    remoteHost="185.10.16.192"
     
     def __init__(self, ttime,cId,dry=False):
         super().__init__()
@@ -29,14 +29,14 @@ class clientProcess(multiprocessing.Process):
         self.toStop=True
     
     def run(self):
-        self.mongoClient = MongoClient(host="mongodb://%s:27017/"%(clientProcess.dbHost))
+        #self.mongoClient = MongoClient(host="mongodb://%s:27017/"%(clientProcess.dbHost))
         while(True):
             st = time.time_ns() // 1_000_000 
             self.think()
             if(not self.dry):
                 self.userLogic()
             end = time.time_ns() // 1_000_000
-            self.mongoClient["client"]["rt"].insert_one({"st":st, "end":end})
+            #self.mongoClient["client"]["rt"].insert_one({"st":st, "end":end})
         
     def userLogic(self):
         print("should subclass")
