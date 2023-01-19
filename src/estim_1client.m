f553c84c5364645b42f1df4377ea94291b16eba7
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-19 18:15:18 +0100

    minor

--- src/estim_1client.m
+++ src/estim_1client.m
@@ -10,7 +10,7 @@ msname=strtrim(string(ms));
 st_est=zeros(1,length(msname));
 
 
-msdata=readData("../data/ICDCS/no_ctrl3/*.csv");
+msdata=readData("../data/ICDCS/*.csv");
 
 %mi mappa gli indici della matrice di routing in quella delle misure che st
 % sto prendendo adesso
@@ -25,29 +25,29 @@ for m=2:length(msname)
     end
 end
 
-% RTm=zeros(1,size(mappingMat,1))-1;
-% Tm=zeros(1,size(mappingMat,1))-1;
-% for i=2:size(mappingMat,1)
-%     %disp([msname(i),msdata(mappingMat(i)).name])
-%     RTm(1,i)=mean(msdata(mappingMat(i)).rt);
-%     Tm(1,i)=mean(msdata(mappingMat(i)).tr);
-% end
+RTm=zeros(1,size(mappingMat,1))-1;
+Tm=zeros(1,size(mappingMat,1))-1;
+for i=2:size(mappingMat,1)
+    %disp([msname(i),msdata(mappingMat(i)).name])
+    RTm(1,i)=mean(msdata(mappingMat(i)).rt);
+    Tm(1,i)=mean(msdata(mappingMat(i)).tr);
+end
 
 
 
 P2=round(P2); 
-for i=1:size(P2,1)
-    for j=1:size(P2,2)
+for i=2:size(P2,1)
+    for j=2:size(P2,2)
         %st_est(i)=st_est(i)+mean(msdata(mappingMat(j)).rt())*P2(i,j);
         st_est(i)=st_est(i)+RTm(1,j)*P2(i,j);
     end
     st_est(i)=RTm(1,i)-st_est(i);
     disp([msname(i),st_est(i)]);
 end
-% 
-% rates=1./(st_est'/1000);
-% 
-% MU=zeros(1,30);
-% MU([5,6,9,12,15,20,23,24,29,30])=rates([3,2,4,5,9,6,10,7,8,1]);
 
-%save("params.mat","MU");
+rates=1./(st_est');
+
+MU=zeros(1,30);
+MU([5,6,9,12,15,20,23,24,29,30])=rates([3,2,4,5,9,6,10,7,8,1]);
+
+save("params.mat","MU");
