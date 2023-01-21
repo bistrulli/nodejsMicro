848672ed594c2e86edc6fdecbccb5eef4311ad78
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-21 12:11:45 +0100

    minot

--- src/getMSTr.m
+++ src/getMSTr.m
@@ -4,9 +4,10 @@ kidx=1;
 tcmp=0;
 tr=[];
 i=1;
-while(i<=size(cmp_evt,1) && length(tr)<=600)
-    disp((cmp_evt(i,2)-min(cmp_evt(:,2)))/10^3)
-    if((kidx-1)<=(cmp_evt(i,2)-min(cmp_evt(:,2)))/10^3 && (cmp_evt(i,2)-min(cmp_evt(:,2)))/10^3<=(kidx))
+initTime=min(cmp_evt(:,1));
+while(i<=size(cmp_evt,1))
+    disp((cmp_evt(i,2)-initTime)/10^3)
+    if((kidx-1)<=(cmp_evt(i,2)-initTime)/10^3 && (cmp_evt(i,2)-initTime)/10^3<=(kidx))
         tcmp=tcmp+1;
         i=i+1;
     else
