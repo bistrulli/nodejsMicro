ceb11dffcba0354181bff3cb76fc373d6ee29652
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-24 08:42:17 +0100

    minor

--- 1tier_spring/ms1/src/main/java/com/example/restservice/MSController.java
+++ 1tier_spring/ms1/src/main/java/com/example/restservice/MSController.java
@@ -1,9 +1,7 @@
 package com.example.restservice;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.commons.math3.distribution.ExponentialDistribution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -14,38 +12,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ctrlmnt.ControllableService;
+
 @RestController
-public class MSController {
-	private static final AtomicInteger users = new AtomicInteger(0);
+public class MSController extends ControllableService {
 
 	@Value("${ms.hw}")
 	private Float hw;
 
 	@Value("${ms.name}")
-	private static String name;
+	private String msname;
+	
+	private static final AtomicInteger users = new AtomicInteger(0);
 
 	@Autowired
 	private MongoTemplate mt;
 
-	private final Long stime = 20l;
+	private final Long stime = 100l;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public ResObj msGet(@RequestParam(name = "login") String login,@RequestParam(name = "name") String name) {
-		System.out.println("Received GET request");
-		System.out.println("%s=%s".formatted(new String[] {"login",login}));
-		System.out.println("%s=%s".formatted(new String[] {"name",name}));
-		this.doWork();
+//		System.out.println("Received GET request");
+//		System.out.println("%s=%s".formatted(new String[] {"login",login}));
+//		System.out.println("%s=%s".formatted(new String[] {"name",name}));
+		System.out.println(this.getHw());
+		this.doWork(this.stime);
 		return new ResObj();
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	@ResponseBody
 	public ResObj msPost(@RequestParam(name = "login") String login,@RequestParam(name = "name") String name) {
-		System.out.println("Received POST request");
-		System.out.println("%s=%s".formatted(new String[] {"login",login}));
-		System.out.println("%s=%s".formatted(new String[] {"name",name}));
-		this.doWork();
+//		System.out.println("Received POST request");
+//		System.out.println("%s=%s".formatted(new String[] {"login",login}));
+//		System.out.println("%s=%s".formatted(new String[] {"name",name}));
+		this.doWork(this.stime);
 		return new ResObj();
 	}
 
@@ -54,22 +56,34 @@ public class MSController {
 		return new ResObj();
 	}
 
-	private void doWork(long stime) {
-		ExponentialDistribution dist = new ExponentialDistribution(stime);
-		Double isTime = dist.getMean();
-		Float d = (float) (isTime.floatValue() * (MSController.users.floatValue() / this.hw));
-		MSController.users.incrementAndGet();
-		try {
-			TimeUnit.MILLISECONDS.sleep(Math.max(Math.round(d), Math.round(isTime)));
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			MSController.users.decrementAndGet();
-		}
+	@Override
+	public Float getHw() {
+		return this.hw;
 	}
 
-	private void doWork() {
-		this.doWork(this.stime);
+	@Override
+	public String getName() {
+		return this.msname;
+	}
+
+	@Override
+	public void setHw(Float hw) {
+		this.hw = hw;
+	}
+
+	@Override
+	public void egress() {
+		MSController.users.decrementAndGet();
+	}
+
+	@Override
+	public Integer getUser() {
+		return MSController.users.get();
+	}
+
+	@Override
+	public void ingress() {
+		MSController.users.incrementAndGet();
 	}
 
 }
