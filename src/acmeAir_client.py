ae54cdb16c325d40b0fd95f89cb1da7bef9a99c1
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-24 14:51:34 +0100

    minor

--- src/acmeAir_client.py
+++ src/acmeAir_client.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
             #data = {"Cli":np.linspace(20,220,25,dtype=int), "RTm":[], "rtCI":[], "Tm":[], "trCI":[], "ms":[],"NC":[]}
             
             
-            data = {"Cli":[1], "RTm":[], "rtCI":[], "Tm":[], "trCI":[], "ms":[],"NC":[]}
+            data = {"Cli":[5], "RTm":[], "rtCI":[], "Tm":[], "trCI":[], "ms":[],"NC":[]}
             
             sys = nodeSys(dbHost=redisHost)
             for p in data["Cli"]:
