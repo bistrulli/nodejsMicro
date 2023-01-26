13609dee2604b37c15f43cc0fe39116706c48f4f
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-26 14:32:45 +0100

    minor

--- src/Client/clientProcess.py
+++ src/Client/clientProcess.py
@@ -11,8 +11,8 @@ class clientProcess(multiprocessing.Process):
     id = None
     mongoClient = None
     ttime = None
-    dbHost="185.10.19.213"
-    remoteHost="185.10.19.213"
+    dbHost="185.154.155.43"
+    remoteHost="185.154.155.43"
     
     def __init__(self, ttime,cId,dry=False):
         super().__init__()
--- src/acmeAir_client.py
+++ src/acmeAir_client.py
@@ -148,7 +148,7 @@ if __name__ == '__main__':
               }
         
         
-        redisHost="185.10.19.213"
+        redisHost="185.154.155.43"
         msNames=list(msSys.keys());
         pedis=redis.StrictRedis(host=redisHost, port=6379, charset="utf-8", decode_responses=True)
         dry=False
