fb600edcf812a0e0ac79a88b9131020d5d5af0c9
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-28 14:41:27 +0100

    data

--- src/plotres.m
+++ src/plotres.m
@@ -3,16 +3,16 @@ clear
 ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl/ctrldata.csv");
 mudata=readData("../data/ICDCS/ctrl/step_ctrl/*.csv");
 
-ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl6/ctrldata.csv");
-mudata2=readData("../data/ICDCS/ctrl/step_ctrl6/*.csv");
+ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl7/ctrldata.csv");
+mudata2=readData("../data/ICDCS/ctrl/step_ctrl7/*.csv");
 
 ctrlMAX=readmatrix("../data/ICDCS/validation/step/ctrldata.csv");
 maxdata=readData("../data/ICDCS/validation/step/*.csv");
 
 
-Tmax = smoothdata(maxdata(end).tr,'movmean',3);
-Tctrl = smoothdata(mudata(end).tr,'movmean',3);
-Tctrl2 = smoothdata(mudata2(end).tr,'movmean',3);
+Tmax = smoothdata(maxdata(end).tr,'movmean');
+Tctrl = smoothdata(mudata(end).tr,'movmean');
+Tctrl2 = smoothdata(mudata2(end).tr,'movmean');
 
 figure
 hold on
