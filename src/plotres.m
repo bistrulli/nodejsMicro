6efce1cc223d37b9a65967bdf3dd9e3eea55fe01
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-29 17:44:31 +0100

    minor

--- src/plotres.m
+++ src/plotres.m
@@ -1,22 +1,31 @@
 clear
 
-ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl/ctrldata.csv");
-mudata=readData("../data/ICDCS/ctrl/step_ctrl/*.csv");
+% ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl/ctrldata.csv");
+% mudata=readData("../data/ICDCS/ctrl/step_ctrl/*.csv");
+% 
+% ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl7/ctrldata.csv");
+% mudata2=readData("../data/ICDCS/ctrl/step_ctrl7/*.csv");
+% 
+% ctrlMAX=readmatrix("../data/ICDCS/validation/step/ctrldata.csv");
+% maxdata=readData("../data/ICDCS/validation/step/*.csv");
+% 
+% 
+% Tmax = smoothdata(maxdata(end).tr,'movmean');
+% Tctrl = smoothdata(mudata(end).tr,'movmean');
+% Tctrl2 = smoothdata(mudata2(end).tr,'movmean');
 
-ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl7/ctrldata.csv");
-mudata2=readData("../data/ICDCS/ctrl/step_ctrl7/*.csv");
 
-ctrlMAX=readmatrix("../data/ICDCS/validation/step/ctrldata.csv");
-maxdata=readData("../data/ICDCS/validation/step/*.csv");
+ctrlmu=readmatrix("../data/ICDCS/ctrl/step_ctrl_fast/ctrldata.csv");
+mudata=readData("../data/ICDCS/ctrl/step_ctrl_fast/*.csv");
 
 
-Tmax = smoothdata(maxdata(end).tr,'movmean');
+% Tmax = smoothdata(maxdata(end).tr,'movmean');
 Tctrl = smoothdata(mudata(end).tr,'movmean');
-Tctrl2 = smoothdata(mudata2(end).tr,'movmean');
+% Tctrl2 = smoothdata(mudata2(end).tr,'movmean');
 
 figure
 hold on
-stairs(Tmax)
-% stairs(Tctrl)
-stairs(Tctrl2)
+% stairs(Tmax)
+stairs(Tctrl)
+% stairs(Tctrl2)
 legend("Ground thruth","muOpt","muOpt2")
