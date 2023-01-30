8c0c7ac5ca68a945d58f3f1dd193889a8437f346
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-30 15:02:16 +0100

    minor

--- src/plotres.m
+++ src/plotres.m
@@ -10,8 +10,8 @@ ctrlMAX=readmatrix("../data/ICDCS/validation/step_fast/ctrldata.csv");
 maxdata=readData("../data/ICDCS/validation/step_fast/*.csv");
 
 
-Tmax = smoothdata(maxdata(1).tr,'movmean',10);
-Tctrl = smoothdata(mudata(1).tr,'movmean',10);
+Tmax = smoothdata(maxdata(1).tr,'movmean',1);
+Tctrl = smoothdata(mudata(1).tr,'movmean',1);
 % Tctrl2 = smoothdata(mudata2(end).tr,'movmean',3);
 
 figure
