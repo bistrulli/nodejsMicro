9f2840f2d808aba7943f0b7d4b5837d42272db4a
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-31 14:29:13 +0100

    data

--- src/plotres.m
+++ src/plotres.m
@@ -1,22 +1,23 @@
 clear
 
-ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl_fast/ctrldata.csv");
-mudata=readData("../data/ICDCS/ctrl/step_ctrl_fast/*.csv");
+% ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl_fast/ctrldata.csv");
+% mudata=readData("../data/ICDCS/ctrl/step_ctrl_fast/*.csv");
 
 % ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl7/ctrldata.csv");
 % mudata2=readData("../data/ICDCS/ctrl/step_ctrl7/*.csv");
 
-ctrlMAX=readmatrix("../data/ICDCS/validation/step_fast/ctrldata.csv");
-maxdata=readData("../data/ICDCS/validation/step_fast/*.csv");
+ctrlMAX=readmatrix("../data/ICDCS/validation/step_gns/ctrldata.csv");
+maxdata=readData("../data/ICDCS/validation/step_gns/*.csv");
 
 
 Tmax = smoothdata(maxdata(1).tr,'movmean',5);
-Tctrl = smoothdata(mudata(1).tr,'movmean',5);
+%Tctrl = smoothdata(mudata(1).tr,'movmean',1);
 % Tctrl2 = smoothdata(mudata2(end).tr,'movmean',3);
 
 figure
 hold on
 stairs(Tmax)
-stairs(Tctrl)
+stairs(ctrlMAX(:,2)*0.77)
+%stairs(Tctrl)
 % stairs(Tctrl2)
 legend("Ground thruth","muOpt")
