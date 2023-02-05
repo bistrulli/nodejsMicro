f60fd559d5448b16824def1549a6083d7b58f9d5
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-05 21:09:27 +0100

    data_gns_150

--- src/plotres.m
+++ src/plotres.m
@@ -1,38 +1,38 @@
 clear
 
-% ctrlMU=readmatrix("../data/ICDCS/ctrl/step_ctrl_fast/ctrldata.csv");
-% mudata=readData("../data/ICDCS/ctrl/step_ctrl_fast/*.csv");
+ctrlMU=readmatrix("../data/ICDCS/ctrl/step_gns_150/ctrldata.csv");
+mudata=readData("../data/ICDCS/ctrl/step_gns_150/*.csv");
 
-ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl_gns2/ctrldata.csv");
-mudata2=readData("../data/ICDCS/ctrl/step_ctrl_gns2/*.csv");
+% ctrlMU2=readmatrix("../data/ICDCS/ctrl/step_ctrl_gns2/ctrldata.csv");
+% mudata2=readData("../data/ICDCS/ctrl/step_ctrl_gns2/*.csv");
 
-ctrlMAX=readmatrix("../data/ICDCS/validation/step_gns2/ctrldata.csv");
-maxdata=readData("../data/ICDCS/validation/step_gns2/*.csv");
+ctrlMAX=readmatrix("../data/ICDCS/validation/step_gns_150/ctrldata.csv");
+maxdata=readData("../data/ICDCS/validation/step_gns_150/*.csv");
 
 Tmax = smoothdata(maxdata(1).tr,'movmean',3);
-%Tctrl = smoothdata(mudata(1).tr,'movmean',1);
-Tctrl2 = smoothdata(mudata2(end).tr,'movmean',3);
+Tctrl = smoothdata(mudata(1).tr,'movmean',3);
+% Tctrl2 = smoothdata(mudata2(end).tr,'movmean',3);
 
 figure
 hold on
 stairs(Tmax)
-stairs(ctrlMU2(:,2)*0.72)
-%stairs(Tctrl)
-stairs(Tctrl2)
+% stairs(ctrlMAX(:,2)*0.66)
+stairs(Tctrl)
+% stairs(Tctrl2)
 legend("Ground thruth","muOpt")
 
 figure
 hold on
-stairs(sum(ctrlMU2(:,3:end),2))
+stairs(ctrlMU(:,3:end))
 
-s=min(size(mudata2(end).rt,1),size(maxdata(end).rt,1));
-x = [mudata2(end).rt(1:s),maxdata(end).rt(1:s)];
+s=min(size(mudata(end).rt,1),size(maxdata(end).rt,1));
+x = [mudata(end).rt(1:s),maxdata(end).rt(1:s)];
 g = [ones(s,1); 2*ones(s,1);];
 figure
 boxplot(x,g)
 
 
-ecdf(mudata2(end).rt)
+ecdf(mudata(end).rt)
 hold on
 ecdf(maxdata(end).rt)
 legend("\mu_{opt}","MAX")
