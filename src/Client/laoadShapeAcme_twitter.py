0e2f6b9d63d1dfc6aaa5ad7275150d93b2cb88bc
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-11 19:54:24 +0100

    data

--- src/Client/laoadShapeAcme_twitter.py
+++ src/Client/laoadShapeAcme_twitter.py
@@ -3,8 +3,8 @@ from scipy.io import loadmat
 
 class loadShapeAcme_twt(loadShape):
     
-    def __init__(self,maxt,sys,dry=False,dbHost="127.0.0.1"):
-        super().__init__(maxt,sys,dry,dbHost)
+    def __init__(self,maxt,sys,dry=False,dbHost="127.0.0.1",datadir=None):
+        super().__init__(maxt,sys,dry,dbHost,datadir)
         self.tweetTrace=loadmat("twitter_20210101_730-24_freq120sec.mat")["tweets"][0]
         self.tweetTrace=(self.tweetTrace-min(self.tweetTrace))/(max(self.tweetTrace)-min(self.tweetTrace))*50+15
         
