ace21f9c2036335e4acdf2a721f5ab506d03678d
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-14 11:46:54 +0100

    data

--- src/acmeAir_ICDCS.py
+++ src/acmeAir_ICDCS.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
         pedis=redis.StrictRedis(host=redisHost, port=6379, charset="utf-8", decode_responses=True)
         dry=False
         
-        for exp in range(1):
+        for exp in range(5):
             
             data = {"Cli":[1], "RTm":[], "rtCI":[], "Tm":[], "trCI":[], "ms":[],"NC":[]}
             sys = nodeSys(dbHost=redisHost)
