8fc002332417fb09d022403be78993408c30e37c
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-15 15:58:47 +0100

    data

--- src/plotres.m
+++ src/plotres.m
@@ -1,9 +1,9 @@
 clear
 
 ctrlGA=zeros(1200,11,1);
-ctrlMU=zeros(600,11,15);
-gaT=zeros(1200,size(ctrlGA,3));
-muT=zeros(600,size(ctrlMU,3));
+ctrlMU=zeros(1200,11,1);
+gaT=zeros(size(ctrlGA,1),size(ctrlGA,3));
+muT=zeros(size(ctrlMU,1),size(ctrlMU,3));
 gadata=[];
 mudata=[];
 valdata=[];
@@ -20,7 +20,7 @@ for i=1:size(ctrlGA,3)
     
     gaRT=[gaRT;gadata(end).rt];
     %gaT=[gaT;gadata(end).tr'];
-    gaT(:,i)=gadata(end).tr(1:1200);
+    gaT(:,i)=gadata(end).tr(1:size(ctrlGA,1));
 
     nanCountGA=sum(isnan(ctrlGA(:,3:end,i)));
     ctrlGA(:,3:end,i)=fillmissing(ctrlGA(:,3:end,i),'constant',ctrlGA(nanCountGA+1:nanCountGA+1,3:end,i));
@@ -28,12 +28,12 @@ end
 
 %load muOpt data
 for i=1:size(ctrlMU,3)
-    ctrlMU(:,:,i)=readmatrix(sprintf("../data/revision2/ctrl/julia_tweeter_%d/ctrldata.csv",i-1));
-    mudata=[mudata;readData(sprintf("../data/revision2/ctrl/julia_tweeter_%d/*.csv",i-1))];
+    ctrlMU(:,:,i)=readmatrix(sprintf("../data/revision2/ctrl/julia_tweeter_7_8_%d/ctrldata.csv",i-1));
+    mudata=[mudata;readData(sprintf("../data/revision2/ctrl/julia_tweeter_7_8_%d/*.csv",i-1))];
     
     muRT=[muRT;mudata(end).rt];
     %muT=[muT;mudata(end).tr'];
-    muT(:,i)=mudata(end).tr(1:600);
+    muT(:,i)=mudata(end).tr(1:size(ctrlMU,1));
    
     nanCountMu=sum(isnan(ctrlMU(:,3:end,i)));
     ctrlMU(:,3:end,i)=fillmissing(ctrlMU(:,3:end,i),'constant',0);
@@ -78,6 +78,7 @@ figure
 stairs(smoothdata(mean(muT,2),'movmean',3));
 hold on
 stairs(smoothdata(mean(gaT,2),'movmean',3));
+legend("muOpt","muGA")
 
 % figure
 % hold on
@@ -102,6 +103,7 @@ figure
 hold on
 ecdf(muRT)
 ecdf(gaRT)
+legend("muOpt","muGA")
 % ecdf(valRT)
 
 
