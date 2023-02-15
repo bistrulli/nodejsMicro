3ff57c24dbf053e66d9d8f3729525dd61cc75e39
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-02-15 23:42:11 +0100

    data

--- src/acmeAir_ICDCS.py
+++ src/acmeAir_ICDCS.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
                 sys.startClient(p,dry=dry)
                 #lancio la forma del carico e i sistemi di monitoring
                 #lshape=loadShapeAcme_const(maxt=1200,sys=sys,dry=dry,dbHost=redisHost,datadir=datadir)
-                lshape=loadShapeAcme_twt(maxt=1200,sys=sys,dry=dry,dbHost=redisHost,datadir=datadir)
+                lshape=loadShapeAcme_twt(maxt=2100,sys=sys,dry=dry,dbHost=redisHost,datadir=datadir)
                 lshape.start()
                 #attendo la fine dell'esperiemnto
                 setStart()
